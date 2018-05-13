d1d84afeeabbb9066938f0b1ea6861dce7cc577e
Sean <seansailer28@gmail.com>
2018-05-12 19:03:52 -0500

    Chart button now has working chart() method, which modifies api_url to the correct format and resets the dropdown selections

--- crypto-webapp/src/app/crypto-dropdown/crypto-dropdown.component.ts
+++ crypto-webapp/src/app/crypto-dropdown/crypto-dropdown.component.ts
@@ -11,10 +11,7 @@ export class CryptoDropdownComponent implements OnInit {
    * Note that this is my private API key and I would appreciate it if it was only used by me. 
    * The API here is free to use, and easy to sign up and obtain your own API key. Thanks in advance.
    */
-  intraday_api_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=EUR&apikey=ZBEjmKdpZAO1LlfQP3Ca";
-  daily_api_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=ZBEjmKdpZAO1LlfQP3Ca";
-  weekly_api_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=CNY&apikey=ZBEjmKdpZAO1LlfQP3Ca";
-  monthly_api_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=CNY&apikey=ZBEjmKdpZAO1LlfQP3Ca";
+  api_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_TIMEFRAME&symbol=FIRSTPAIR&market=SECONDPAIR&apikey=ZBEjmKdpZAO1LlfQP3Ca";
   first_pair = "";
   second_pair = "";
   timeframe = "";
@@ -34,5 +31,19 @@ export class CryptoDropdownComponent implements OnInit {
 
   updateTimeframe(time : string){
     this.timeframe = time;
+  }
+
+  chartPairing(){
+    if (this.first_pair.length == 0 || this.second_pair.length == 0 || this.timeframe.length == 0){
+      return;
+    }
+    this.api_url = this.api_url.replace("TIMEFRAME", this.timeframe);
+    this.api_url = this.api_url.replace("FIRSTPAIR", this.first_pair);
+    this.api_url = this.api_url.replace("SECONDPAIR", this.second_pair);
+    console.log(this.api_url);
+
+    this.first_pair = "";
+    this.second_pair = "";
+    this.timeframe = "";
   }
 }
